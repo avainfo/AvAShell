72e802c2cf9643208b6d7cc0f3e9714af4d041c3
avainfo <avainfoconseil@gmail.com>
2023-09-17 11:48:02 +0200

    feature-001: setup Window on Windows + Linux

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,26 @@
-import 'package:ava_shell/pages/main_page.dart';
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-void main() {
+import 'package:ava_shell/pages/main_page.dart';
+import 'package:bitsdojo_window/bitsdojo_window.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_acrylic/flutter_acrylic.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Window.initialize();
+  if (Platform.isWindows) {
+    await Window.hideWindowControls();
+  }
   runApp(const MyApp());
+  if (Platform.isWindows) {
+    doWhenWindowReady(() {
+      appWindow
+        ..minSize = const Size(640, 360)
+        ..size = const Size(720, 540)
+        ..alignment = Alignment.center
+        ..show();
+    });
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -11,6 +29,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'AVA Shell',
       home: MainPage(),
     );
